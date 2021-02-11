055c2e107045249538850d71b7ccabd17852a8d7
Matt Yamarino <779649@cognizant.com>
2021-02-11 16:18:03 -0600

    product controller setup

--- src/test/java/com/galvanize/clothingstore/controller/JacketControllerTest.java
+++ src/test/java/com/galvanize/clothingstore/controller/JacketControllerTest.java
@@ -1,8 +1,12 @@
 package com.galvanize.clothingstore.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.clothingstore.repository.JacketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,4 +14,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs(outputDir = "target/snippets")
 class JacketControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    JacketRepository jacketRepository;
 }
