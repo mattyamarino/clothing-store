40c7d8acf2e528a58acffe937ebbbdc650ae890b
Matt Yamarino <779649@cognizant.com>
2021-02-12 09:55:23 -0600

    finished controller method to update product

--- src/main/java/com/galvanize/clothingstore/controller/JacketController.java
+++ src/main/java/com/galvanize/clothingstore/controller/JacketController.java
@@ -1,15 +1,22 @@
 package com.galvanize.clothingstore.controller;
-
 import com.galvanize.clothingstore.model.JacketEntity;
 import com.galvanize.clothingstore.service.JacketService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products/")
 public class JacketController {
+    private JacketService jacketService;
+
+    public JacketController(JacketService jacketService) {
+        this.jacketService = jacketService;
+    }
+
+    @PutMapping("jacket/{productId}")
+    public void updateJacket(@PathVariable Long productId,
+                              @RequestBody JacketEntity objectToUpdate) {
+        jacketService.updateProduct("jacket", productId, objectToUpdate);
+    }
 }
 
 
--- src/test/java/com/galvanize/clothingstore/controller/JacketControllerTest.java
+++ src/test/java/com/galvanize/clothingstore/controller/JacketControllerTest.java
@@ -1,14 +1,21 @@
 package com.galvanize.clothingstore.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.clothingstore.model.JacketEntity;
+import com.galvanize.clothingstore.model.Season;
 import com.galvanize.clothingstore.repository.JacketRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,4 +30,25 @@ class JacketControllerTest {
 
     @Autowired
     JacketRepository jacketRepository;
+
+    @Test
+    public void updateJacket_updatesJacket() throws Exception {
+        JacketEntity jacketEntity = jacketRepository.save(new JacketEntity(Season.SPRING, "M",
+                "Blue", "Awesome", true, 10L));
+
+        JacketEntity jacketToUpdate = new JacketEntity(Season.SUMMER, "L",
+                "Blue", "Awesome", true, 10L);
+
+        objectMapper = new ObjectMapper();
+        String jacketString = objectMapper.writeValueAsString(jacketToUpdate);
+
+        mockMvc.perform(put("/api/products/jacket/" + jacketEntity.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jacketString))
+                .andExpect(status().isOk());
+
+        jacketToUpdate.setId(jacketEntity.getId());
+        JacketEntity result = jacketRepository.findById(jacketEntity.getId()).get();
+        assertEquals(jacketToUpdate, result);
+    }
 }
